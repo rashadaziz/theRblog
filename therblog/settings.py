043cae68acd9e6ad7a0e8e885b401cb512b56c60
rashadaziz <rashad.aziz@ui.ac.id>
2022-03-07 10:33:20 +0700

    heroku deploy test

--- therblog/settings.py
+++ therblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 import os
 from pathlib import Path
 
@@ -127,3 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
