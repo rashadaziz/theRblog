8a07aceff84e79a4b19d3ac93325e4f76ae713ec
rashadaziz <rashad.aziz@ui.ac.id>
2022-03-10 19:54:57 +0700

    authentication backend functional

--- main/static/js/page-art.js
+++ main/static/js/page-art.js
@@ -3,11 +3,9 @@ const context = canvas.getContext("2d");
 const container = document.querySelector(".page-banner");
 canvas.width = container.getBoundingClientRect().width;
 canvas.height = container.getBoundingClientRect().height;
-
-const image1 = new Image();
-image1.src = "../images/image1.jpg"
-const image2 = new Image();
-image2.src = "../images/image2.jpg"
+const images = document.querySelectorAll("img");
+const image1 = images[0];
+const image2 = images[1];
 
 addEventListener("resize", () => {
   canvas.width = container.getBoundingClientRect().width;
@@ -15,28 +13,56 @@ addEventListener("resize", () => {
 });
 
 function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    drawEllipse("#b1a7a6", 0.2, 200, 100)
-    drawEllipse("#ba181b", 0.8, 300, 100)
-    drawEllipse("#FFFF", 0, 300, 50)
-    requestAnimationFrame(draw)
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawEllipse("#b1a7a6", 0.2, 300, 100);
+  drawEllipse("#ba181b", 0.8, 400, 100);
+  drawEllipse("#FFFF", 0, 400, 50);
+  drawImage(image1, 170, 120, 250, 350, 10);
+  drawImage(image2, 390, 150, 250, 350, 10);
+  requestAnimationFrame(draw);
+}
+
+function drawImage(image, x, y, width, height, borderRadius) {
+  // x and y are start coordinates
+  context.save();
+  roundedRect(x, y, width, height, borderRadius);
+  context.clip();
+  context.drawImage(image, x, y, width, height);
+  context.restore();
 }
 
 function drawEllipse(color, rotation, width, height) {
-    context.beginPath();
-    context.ellipse(
-      canvas.width * 0.5 + rotation,
-      canvas.height * 0.5,
-      width,
-      height,
-      Math.PI * rotation,
-      0,
-      Math.PI * 2
-    );
-    context.fillStyle = color
-    context.fill();
+  context.beginPath();
+  context.ellipse(
+    canvas.width * 0.5 + rotation,
+    canvas.height * 0.5,
+    width,
+    height,
+    Math.PI * rotation,
+    0,
+    Math.PI * 2
+  );
+  context.fillStyle = color;
+  context.fill();
+}
+
+function roundedRect(x, y, width, height, radius) {
+  context.beginPath();
+  context.moveTo(x + radius, y);
+  context.lineTo(x + width - radius, y);
+  context.quadraticCurveTo(x + width, y, x + width, y + radius);
+  context.lineTo(x + width, y + height - radius);
+  context.quadraticCurveTo(
+    x + width,
+    y + height,
+    x + width - radius,
+    y + height
+  );
+  context.lineTo(x + radius, y + height);
+  context.quadraticCurveTo(x, y + height, x, y + height - radius);
+  context.lineTo(x, y + radius);
+  context.quadraticCurveTo(x, y, x + radius, y);
+  context.closePath();
 }
 
 draw();
-
-
--- user/views.py
+++ user/views.py
@@ -1,24 +1,20 @@
 from django.db import IntegrityError
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect, render
 from .models import User
 
 def register(request):
     context = {}
-
     if request.method == "POST":
         username = request.POST["username"]
         full_name = request.POST["full_name"]
         password = request.POST["password"]
-        remember_me = request.POST.get("remember_me")
         user = User(username=username, full_name=full_name)
         user.set_password(password)
 
         try:
             user.save(force_insert=True)
             request.session.clear()
-            if not remember_me:
-                request.session.set_expiry(0)
             login(request, user)
             return redirect("home")
         
@@ -26,3 +22,30 @@ def register(request):
             context["duplicate_email"] = True
 
     return render(request, "register.html", context)
+
+
+def login_view(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        remember_me = request.POST.get("remember_me")
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            if not remember_me:
+                request.session.set_expiry(0)
+            login(request, user)
+            return redirect("home")
+
+        else:
+            context["login_error"] = True
+    
+    return render(request, "login.html", context)
+
+def logout_view(request):
+    logout(request)
+    request.session.flush()
+    return redirect("home")
+    
+
